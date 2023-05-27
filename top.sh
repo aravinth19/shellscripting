b0640bfeea643bfa40fd765c5d559a1181c8fa8b
Aravinth Shanmugam <42541328+aravinth19@users.noreply.github.com>
2023-05-27 12:39:27 +0530

    Update top.sh

--- top.sh
+++ top.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 
-top
+# Check CPU usage
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+
+echo "CPU Usage: $cpu_usage"
+echo "CPU usage is higher than $THRESHOLD_CPU%, it's $cpu_usage%"
